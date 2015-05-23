9cf89e5335f6b9448e6e448fa4ae9233ee760d2e
Russell Todd <russell.todd@northpoint.org>
2015-05-23 10:17:49 -0400

    gets it to at least not throw exceptions

--- src/Checks/FlysystemHealthCheck.php
+++ src/Checks/FlysystemHealthCheck.php
@@ -8,11 +8,11 @@ use League\Flysystem\FileSystem;
  * When using Flysystem directly, this check makes sure
  * each configured connection is working.
  */
-class FlysystemHealthCheck implements HealthCheckInterface {
+class FlysystemHealthCheck extends AbstractHealthCheck {
 
     protected $flysystem;
 
-    public function __construct( $config ) {
+    public function configure($config ) {
         \Log::debug(__METHOD__.'(' . print_r($config,true) . ')');
         $this->flysystem = $this->createFlysystem( $config );
     }
@@ -20,20 +20,23 @@ class FlysystemHealthCheck implements HealthCheckInterface {
     protected function createFlysystem( $config ) {
         if (is_array($config)) {
             if (array_key_exists('driver',$config)) {
-                $adapterClass = array_pull($config,'driver');
+                $driver = array_pull($config,'driver');
             } else if (array_key_exists('adapter',$config)) {
-                $adapterClass = array_pull($config,'adapter');
+                $driver = array_pull($config,'adapter');
             } else {
                 // misconfigured
                 throw new Exception('Must specify the Flystem adapter in the configuration, but configured keys are ['.implode(",", array_keys($config)). ']');
             }
         } else if (is_string($config)) {
             // using a config from Laravel's filesystem
-            $config = \Config::get('filesystem.disks.' . $config);
-            $adapterClass = array_pull($config,'driver');
+            $fsConfig = \Config::get('filesystems.disks');
+            \Log::debug(__METHOD__.':: got filesystem config: '.print_r($fsConfig,true));
+            $driver = array_pull($fsConfig[$config],'driver');
+            $config = $fsConfig;
         }
 
-        \Log::debug(__METHOD__.':: now instantiating a Flysystem with adapter '.$adapterClass);
+        $adapterClass = $this->getAdapterForDriver($driver);
+        \Log::debug(__METHOD__.':: now instantiating a Flysystem for '.$driver.'with adapter '.$adapterClass);
 
         $reflection = new \ReflectionClass($adapterClass);
         $inst = new Filesystem ( $reflection->newInstance( $config ) );
@@ -52,6 +55,25 @@ class FlysystemHealthCheck implements HealthCheckInterface {
         } catch( Exception $e ) {
             return false;
         }
+    }
+
+    /*
+     * maps the short driver name to the full class for Flysystem Adapter
+     */
+    public function getAdapterForDriver($driver) {
+        switch($driver) {
+            case 'ftp':
+                return 'League\Flysystem\Adapter\Ftp';
+            case 'sftp':
+                return 'League\Flysystem\Sftp\SftpAdapter';
+            case 'local':
+                return 'League\Flysystem\Adapter\Local';
+            case 'rackspace':
+                return 'League\Flysystem\Rackspace\RackspaceAdapter';
+            default:
+                throw new Exception('Driver not supported: '.$driver);
+        }
+
     }
 
 }
